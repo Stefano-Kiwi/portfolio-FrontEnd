c415ec9bc563d017c452a20b7f5fd81cbde06dc7
Stefano-Kiwi <stefanobudon@gmail.com>
2023-05-14 23:50:04 -0300

    Modifico para firebase

--- src/app/service/serv-experiencia.service.ts
+++ src/app/service/serv-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ServExperienciaService {
-  expURL = 'http://localhost:8080/explab/'
+  expURL = 'https://backstefano.onrender.com/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -8,7 +8,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = 'http://localhost:8080/skill/';
+  URL = 'https://backstefano.onrender.com/skill/';
   
   constructor(private httpClient: HttpClient) { }
 
